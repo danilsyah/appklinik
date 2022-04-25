dcfa44ddf6f31688b823b543d1b81d9e0b9d7197
danilsyah <danilsyaharihardjo@gmail.com>
2022-04-25 15:05:47 +0700

    upload templates

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '94D@nilsyah'
+
+@app.route('/')
+def index():
+    return render_template('dashboard.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
